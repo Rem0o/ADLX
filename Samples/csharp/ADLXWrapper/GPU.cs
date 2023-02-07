757f0559cf7ed55e8340638f44fe408e7b6e0915
Rem0o <rem.merc@outlook.com>
2023-02-07 11:37:28 -0500

    Update GPU.cs

--- Samples/csharp/ADLXWrapper/GPU.cs
+++ Samples/csharp/ADLXWrapper/GPU.cs
@@ -4,14 +4,12 @@ namespace ADLXWrapper
 {
     public class GPU : UnmanagedWrapper<IADLXGPU>
     {
-        private IADLXGPU _gpu;
-
         public GPU(IADLXGPU gpu) : base(gpu)
         {
             using (var disposable = new CompositeDisposable())
             {
                 var ptr = ADLX.new_stringP_Ptr().DisposeWith(ADLX.delete_stringP_Ptr, disposable);
-                _gpu.Name(ptr).ThrowIfError("Couldn't get GPU name");
+                UnmanagedInterface.Name(ptr).ThrowIfError("Couldn't get GPU name");
 
                 Name = ADLX.stringP_Ptr_value(ptr);
             }
